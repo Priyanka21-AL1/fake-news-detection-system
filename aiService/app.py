47b07aee07d2d7bf6d21b2151c265295e5078761
Priyanka Bangar <bangarpriya312@gmail.com>
2026-06-23 10:15:52 +0530

    FastAPI prediction API completed

--- aiService/app.py
+++ aiService/app.py
@@ -1,9 +1,40 @@
 # app.py
 
 from fastapi import FastAPI
+from pydantic import BaseModel
+import joblib
+
+# Load trained model
+model = joblib.load("model/model.pkl")
+vectorizer = joblib.load("model/vectorizer.pkl")
 
 app = FastAPI()
 
+# Request Body
+class NewsRequest(BaseModel):
+    text: str
+
 @app.get("/")
 def home():
-    return {"message":"AI Service Running"}
+    return {"message": "Fake News Detection API Running"}
+
+@app.post("/predict")
+def predict(news: NewsRequest):
+
+    text_vector = vectorizer.transform([news.text])
+
+    prediction = model.predict(text_vector)[0]
+
+    probability = model.predict_proba(text_vector)
+
+    confidence = round(
+        max(probability[0]) * 100,
+        2
+    )
+
+    result = "Fake" if prediction == 0 else "Real"
+
+    return {
+        "prediction": result,
+        "confidence": f"{confidence}%"
+    }
--- aiService/test_model.py
+++ aiService/test_model.py
@@ -3,12 +3,12 @@ import joblib
 loaded_model = joblib.load("model/model.pkl")
 loaded_vectorizer = joblib.load("model/vectorizer.pkl")
 
-sample = [
+sample_news = [
     "Government launches new education policy."
 ]
 
-sample_vector = loaded_vectorizer.transform(sample)
+sample_vector = loaded_vectorizer.transform(sample_news)
 
 prediction = loaded_model.predict(sample_vector)
 
-print(prediction)
+print("Prediction:", prediction[0])
